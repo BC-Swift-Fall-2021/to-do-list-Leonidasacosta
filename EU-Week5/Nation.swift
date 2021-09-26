8fe82ff031ec9fe42e99a99ef7e8d97a7574a933
Leonidasacosta <89566738+Leonidasacosta@users.noreply.github.com>
2021-09-26 02:28:50 -0400

    Completed Video 5.6 Updated EU!

---
+++ EU-Week5/Nation.swift
@@ -0,0 +1,14 @@
+//
+//  Nation.swift
+//  EU-Week5
+//
+//  Created by Leonidas Acosta on 9/26/21.
+//
+
+import Foundation
+
+struct Nation {
+    var country: String
+    var capital: String
+    var usesEuro: Bool
+}
