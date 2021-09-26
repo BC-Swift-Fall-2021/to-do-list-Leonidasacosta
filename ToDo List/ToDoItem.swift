fbc225a0a6c1e175441fc97cbaa8b77e32b3525e
Leonidasacosta <89566738+Leonidasacosta@users.noreply.github.com>
2021-09-26 02:29:48 -0400

    Completed Video 5.6 Updated ToDoList!

---
+++ ToDo List/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  ToDo List
+//
+//  Created by Leonidas Acosta on 9/26/21.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
